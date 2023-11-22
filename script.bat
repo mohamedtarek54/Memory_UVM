8cd96f5700e4e952199641073321e4ed8f8881e0
Mohamed Tarek <mohammedtarek54@gmail.com>
2023-11-22 14:09:31 +0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,10 +1,16 @@
+:: 
 :: configure shell window
+::
 color C
 title Compiling Memory UVM
 echo off
 cls
 echo;
 
+
+::
+:: explaining whats happening in this script
+::
 echo script works as follow:
 echo;
 echo - delete any /work directory exists in current path
@@ -20,25 +26,41 @@ echo - after simulation window is closed, all logs are deleted(transcript, vsim.
 echo;
 pause
 
+
+::
+:: Deleting previous work directory (if exists)
+::
 echo deleting work directory...
 RD /S /Q work
 echo done
 echo;
 
+::
+:: Create new work directory 
+::
 echo creating a new work directory...
 vlib work
 echo done
 echo;
 
-echo compiling all verilog files
+::
+:: compile systemverilog file
+::
+echo compiling all systemverilog files
 vlog memory_pkg.sv top.sv intf.sv memory_16x32.sv +cover
 echo done
 echo;
 
+::
+:: open simulation with the defined configurations 
+::
 echo opening simulation...
 vsim top -coverage -do "set NoQuitOnFinish 1; add wave -position insertpoint sim:/top/intf1/*; run -all; coverage report -codeAll -cvg -verbose"
 echo;
 
+::
+:: delete all generated logs and files
+::
 echo simulation is closed
 echo deleting simulation logs...
 DEL transcript
